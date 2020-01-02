bdb5ad75499c3260cf76fa650a9cec1fc4781642
Krzysztof Śmigrodzki <Krzysztof.Smigrodzki@mf.gov.pl>
2020-01-02 13:40:27 +0100

    Formatowanie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TaskListComponent } from './tasks/task-list/task-list.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { EnterTaskComponent } from './tasks/enter-task/enter-task.component';
-import { CheckboxComponent } from './ui/checkbox/checkbox.component';
+import {AppComponent} from './app.component';
+import {TaskListComponent} from './tasks/task-list/task-list.component';
+import {TaskComponent} from './tasks/task/task.component';
+import {EnterTaskComponent} from './tasks/enter-task/enter-task.component';
+import {CheckboxComponent} from './ui/checkbox/checkbox.component';
 import {TaskService} from './tasks/task.service';
-import { ToggleComponent } from './ui/toggle/toggle.component';
+import {ToggleComponent} from './ui/toggle/toggle.component';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {Database} from './database';
-import { TaskListContainerComponent } from './tasks/task-list-container/task-list-container.component';
+import {TaskListContainerComponent} from './tasks/task-list-container/task-list-container.component';
 import {ProjectService} from './project/project.service';
 
 @NgModule({
@@ -32,4 +32,5 @@ import {ProjectService} from './project/project.service';
   providers: [ProjectService, TaskService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
